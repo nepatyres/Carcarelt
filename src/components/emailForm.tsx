6c5c7017e9255aa5f983837cf3aed17388038333
nepatyres <contactjulijus@gmail.com>
2024-07-17 10:11:29 +0300

    patch 2.7

--- src/components/emailForm.tsx
+++ src/components/emailForm.tsx
@@ -20,7 +20,7 @@ export default function EmailForm() {
                         <input type="phone" className="bg-black text-white focus:bg-black py-[6px] px-3 focus:outline-2 focus:outline-blue-400 rounded-md focus:text-white border border-white w-full" id="exampleFormControlInput3" placeholder="Phone" required />
                     </div>
                     <div className="mb-3">
-                        <textarea className="bg-black text-white focus:bg-black py-[6px] px-3 focus:outline-2 focus:outline-blue-400 rounded-md focus:text-white border border-white w-full" id="exampleFormControlTextarea1" rows="3" placeholder="Your message" required></textarea>
+                        <textarea className="bg-black text-white focus:bg-black py-[6px] px-3 focus:outline-2 focus:outline-blue-400 rounded-md focus:text-white border border-white w-full" id="exampleFormControlTextarea1" rows={3} placeholder="Your message" required></textarea>
                     </div>
                     <div className='email-btn'>
                         <button className='font-ubuntuL font-thin text-[20px] bg-transparent hover:bg-[#c1d8f026] transition-colors ease-in duration-500 bg-black text-white focus:bg-black focus:text-white border border-[#c1d8f0cc] rounded-[5px] mt-0 ml-0.5 py-px px-2' type="submit">
